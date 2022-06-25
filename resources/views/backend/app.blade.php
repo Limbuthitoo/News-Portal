600b095cc901872d83a83bad04f3a420e9bd4bba
Bijay Subba Limbu <bijaysubbalimbu@gmail.com>
2022-06-25 13:45:38 +0545

    minor cc added

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -261,10 +261,8 @@
                 <footer class="footer">
                     <div class="d-sm-flex justify-content-center justify-content-sm-between">
                         <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©
-                            bootstrapdash.com 2020</span>
-                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a
-                                href="https://www.bootstrapdash.com/bootstrap-admin-template/"
-                                target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+                            Nepal News Pvt Ltd, 2020-2022</span>
+                        <
                     </div>
                 </footer>
                 <!-- partial -->
